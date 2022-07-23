b6da8ccd60e2308561029b3ae307aa05240f9e18
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-07-23 15:38:39 +0200

    add components to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { gsap } from 'gsap'
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,20 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // GSAP
+    // gsap.from(".title", {
+    //   duration: 3,
+    //   x: "-50%",
+    //   opacity: 0,
+    //   ease: "expo.out",
+    // });
+  
+    // gsap.from(".subtitle", {
+    //   duration: 3,
+    //   opacity: 0,
+    //   delay: 2,
+    //   ease: "expo.out",
+    // });
   }
 
 }
