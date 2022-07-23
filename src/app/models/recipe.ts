b6862ad537e64aca6137b616f1d7784696e7a4e8
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-07-23 11:52:34 +0200

    add models

---
+++ src/app/models/recipe.ts
@@ -0,0 +1,9 @@
+export interface Guide {
+    title: string;
+    description: string;
+    cheapTag: boolean;
+    easyTag: boolean;
+    quickTag: boolean;
+    lessWashTag: boolean;
+    temperatureTag: boolean;
+}
