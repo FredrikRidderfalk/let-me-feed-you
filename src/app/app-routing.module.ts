0ca9f50b0ea3052f47ffe69f9a0a97c1a5d5a3cb
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-07-23 11:45:34 +0200

    generate pages & components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { AcademyComponent } from './pages/academy/academy.component';
+import { StoriesComponent } from './pages/stories/stories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "recipes", component: RecipesComponent },
+  { path: "academy", component: AcademyComponent },
+  { path: "stories", component: StoriesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { AcademyComponent } from './pages/academy/academy.component';
+import { StoriesComponent } from './pages/stories/stories.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { GuideComponent } from './components/guide/guide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    RecipesComponent,
+    AcademyComponent,
+    StoriesComponent,
+    RecipeComponent,
+    GuideComponent
   ],
   imports: [
     BrowserModule,
