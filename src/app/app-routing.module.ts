52b4cd752c70b4cf0732d59f81ca0d84a11ca0fe
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-08-05 07:06:13 +0200

    add hotteok routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,14 @@ import { StoriesComponent } from './pages/stories/stories.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { OkonomiyakiComponent } from './pages/recipes/foods/okonomiyaki/okonomiyaki.component';
 import { BurgersComponent } from './pages/recipes/foods/burgers/burgers.component';
+import { HotteokComponent } from './pages/recipes/foods/hotteok/hotteok.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "recipes", component: RecipesComponent },
   { path: "recipe", component: RecipeComponent },
   { path: "recipes/okonomiyaki", component: OkonomiyakiComponent },
-  // { path: "recipes/hotteok", component: HotteokComponent },
+  { path: "recipes/hotteok", component: HotteokComponent },
   { path: "recipes/burgers", component: BurgersComponent },
   { path: "academy", component: AcademyComponent },
   { path: "stories", component: StoriesComponent },
