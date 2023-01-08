7c7a762942e039ea44328980b1b5c12a88ba1805
Fredrik Ridderfalk <ridderfalk@gmail.com>
2023-01-08 19:09:39 +0100

    style: update dark-mode styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -19,6 +19,9 @@ export class NavbarComponent implements OnInit {
     const localStorageTheme = localStorage.getItem("user_theme");
     if (localStorageTheme) {
       this.userTheme = localStorageTheme
+      if(localStorageTheme === "dark_mode") {
+        this.toggleDarkMode()
+      }
     }
   }
 
@@ -26,6 +29,10 @@ export class NavbarComponent implements OnInit {
     localStorage.setItem("user_theme", this.userTheme == "dark_mode" ? "light_mode" : "dark_mode");
     const prevTheme = localStorage.getItem("user_theme");
     this.userTheme = prevTheme
+    this.toggleDarkMode()
+  }
+
+  toggleDarkMode() {
     document.body.classList.toggle('dark_mode');
   }
 
